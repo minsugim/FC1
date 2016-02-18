a35828d6679ac3bbe09d63f13480aa69b68fe6c2
Jonathan <jyc24@uw.edu>
2016-02-18 15:13:15 -0800

    added my movies yooooooo

--- movies_here.R
+++ movies_here.R
@@ -4,3 +4,4 @@ Minsu_Movies <- c("Tangled", "The Dark Knight", "Bourne Supremacy", "Iron Man", 
 
 Eric <- c('Lilo and Stich', 'Deadpool', 'Avatar', 'Movie 4', 'Movie 5')
 
+Jonathan <-c('Deadpool', 'How to train your dragon 1', 'how to train your dragon 2')
